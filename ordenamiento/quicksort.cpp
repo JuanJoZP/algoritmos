3c5a5863efba30c86d23a6daf194c7c0b5650dfb
JuanJoZP <juanzpjose136@gmail.com>
2022-09-19 08:46:21 -0500

    Update quicksort.cpp

--- ordenamiento/quicksort.cpp
+++ ordenamiento/quicksort.cpp
@@ -1,50 +1,39 @@
+// Online C++ compiler to run C++ program online
 #include <iostream>
 #include <vector>
 using namespace std;
 
-void quicksort(vector<int>& v) {
-    int i = -1;
-    if(v.size() > 1) {
-        int last = v.size()-1;
-        int swap;
-        for(int j = 0; j < v.size()-1; j++) {
-            if(v.at(j) < v.at(last)) {
-                swap = v.at(j);
-                v.at(j) = v.at(++i);
-                v.at(i) = swap;
+void heapsort(vector<int> &v) {
+    int left, right, n, last_parent;
+    for(int i = 0; i < v.size(); i++) {
+        n = v.size()-i-1;
+        // se usa formula diferente si el vector es par o impar
+        last_parent = (n%2 == 0) ? n/2 - 1 : (n+1)/2 - 1;
+        
+        for(int i = last_parent; i >= 0; i--) {
+            left = 2*(i+1)-1;
+            right = 2*(i+1);
+            
+            if(v.at(i) < v.at(left)) {
+                swap(v.at(i), v.at(left));
+            }
+            
+            // no mira el nodo derecho del ultimo arbol cuando el vector es impar
+            if((i==last_parent && n%2 == 0) || i!=last_parent) {
+                if(v.at(i) < v.at(right)) {
+                    swap(v.at(i), v.at(right));
+                }
             }
         }
-        swap = v.at(++i);
-        v.at(i) = v.at(last);
-        v.at(last) = swap;
-        
-        vector<int> lp (v.begin(), v.begin() + i);
-        vector<int> rp (v.begin()+i+1, v.end());
-        
-        quicksort(lp);
-        int pivot = v.at(i);
-        quicksort(rp);
-        
-        v.clear();
-        for(int h = 0; h < lp.size(); h++) {
-            v.push_back(lp.at(h));
-        }
-        v.push_back(pivot);
-        for(int h = 0; h < rp.size(); h++) {
-            v.push_back(rp.at(h));
-        }
-        
+        swap(v.at(0), v.at(n));
     }
 }
 
 int main() {
-    vector<int> v1 {6,1,59,9,7,2,5,4,3,0, 27};
-    quicksort(v1);
-
-    for(int h = 0; h < v1.size(); h++){
-        cout << v1.at(h) << ", ";
+    vector<int> v1 {4,2,7,1,5,9,8,3,6,5,7,3,12,343};
+    heapsort(v1);
+    
+    for(int i = 0; i < v1.size(); i++) {
+        cout << v1.at(i) << ", ";
     }
-    cout << endl;
-
-    return 0;
 }
