3f6c809004292b74846ee200a94992599e7438f7
JuanJoZP <juanzpjose136@gmail.com>
2022-11-04 08:26:15 -0500

    rotacion izq-izq y der-der

--- estructuras/tree_avl.cpp
+++ estructuras/tree_avl.cpp
@@ -75,21 +75,61 @@ class Tree {
     
 public:
     
-    void add(int d) {
-        Node* n = new Node(d);
-        if(root == NULL) {
-            root = n;
-        } else {
-            Node* t = root;
-            Node* hoja = searchLeaf(t, n->getData());
+    void addr(int d){
+      if(root != NULL)
+        addr(d, root, root);
+      else
+        root =  new Node(d);
+    }
+
+    void addr(int d, Node* t, Node* pt){
+        if(t == NULL){
+            Node* n = new Node(d);
+            if(d < pt->getData()){
+                pt->setIzq(n);
+            }else{
+                pt->setDer(n);
+            }
+        }else{
+            if(d < t->getData()){
+                addr(d, t->getIzq(), t);
+            }else{
+                addr(d, t->getDer(), t);
+            }
             
-            if(n->getData() >= hoja->getData()) {
-                hoja->setDer(n);
-            } else {
-                hoja->setIzq(n);
+            if(!isBalanced(t)) {
+                if(isIzqHeavy(t)) {
+                    if(isHijoIzq(d)) {
+                        Node *z = t->getIzq();
+                        Node *pt = parent(t->getData());
+                        t->setIzq(z->getDer());
+                        z->setDer(t);
+                        
+                        if(pt == NULL) {
+                            root = z;
+                        } else {
+                            pt->setIzq(z);
+                        }
+                    }
+                } else if(isDerHeavy(t)) {
+                    if(isHijoDer(d)) {
+                        Node *z = t->getDer();
+                        Node *pt = parent(t->getData());
+                        t->setDer(z->getIzq());
+                        z->setIzq(t);
+                        
+                        if(pt == NULL) {
+                            root = z;
+                        } else {
+                            pt->setDer(z);
+                        }
+                    }
+                }
             }
         }
     }
+            
+
     
     int getHeight(Node *r) {
         if(r == NULL) {
@@ -119,18 +159,44 @@ public:
         return isIzqHeavy(root);
     }
     
+    bool isDerHeavy(Node *r) {
+        return getHeight(r->getIzq())-getHeight(r->getDer()) < -1;
+    }
+    
+    bool isDerHeavy() {
+        return isDerHeavy(root);
+    }
+    
     Node* parent(int ch, Node *t) {
-        bool izq = ch < t->getData();
-        
-        if (izq) {
-            if(t->getIzq()->getData() == ch) return t;
-            return parent(ch, t->getIzq());
+        if(t == NULL) {
+            return NULL;
         } else {
-            if(t->getDer()->getData() == ch) return t;
-            return parent(ch, t->getDer());
+            if(t->getData() == ch) {
+                return NULL;
+            } else {
+                if(t->getData() > ch) {
+                    if(t->hasIzq()) {
+                        if(t->getIzq()->getData() == ch) {
+                            return t;
+                        } else {
+                            return parent(ch, t->getIzq());
+                        }
+                    } else {
+                        return NULL;
+                    }
+                } else {
+                    if(t->hasDer()) {
+                        if(t->getDer()->getData() == ch) {
+                            return t;
+                        } else {
+                            return parent(ch, t->getDer());
+                        }
+                    } else {
+                        return NULL;
+                    }
+                }
+            }
         }
-        
-        return NULL;
     }
     
     Node* parent(int ch) {
@@ -141,6 +207,12 @@ public:
         Node* t = parent(ch);
         
         return t->getIzq()->getData() == ch;
+    }
+    
+    bool isHijoDer(int ch) {
+        Node* t = parent(ch);
+        
+        return t->getDer()->getData() == ch;
     }
     
     void preorder() {
@@ -172,19 +244,25 @@ int main() {
     // Write C++ code here
     Tree tree = Tree();
     
-    tree.add(18);
-    tree.add(12);
-    tree.add(8);
-    tree.add(7);
-    tree.add(6);
-    tree.add(15);
-    tree.add(24);
-    tree.add(21);
-    tree.add(41);
+    tree.addr(20);
+    tree.addr(15);
+    tree.addr(24);
+    tree.addr(10);
+    tree.addr(22);
+    tree.addr(31);
+    tree.addr(18);
+    tree.addr(11);
+    tree.addr(7);
 
+    tree.preorder();
+    cout << endl;
+    
+    tree.addr(40);
+    tree.addr(50);
     
     tree.preorder();
     cout << endl;
+
 
     //cout << tree.getHeight() << endl;
     //cout << tree.isBalanced() << endl;
